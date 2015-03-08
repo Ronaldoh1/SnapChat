3d73ae98c4d8b107c017aae87d7abe581d38aabf
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-07 22:34:25 -0800

    Initial Commit

---
+++ SnapChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChat
+//
+//  Created by Ronald Hernandez on 3/7/15.
+//  Copyright (c) 2015 Ronald Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
